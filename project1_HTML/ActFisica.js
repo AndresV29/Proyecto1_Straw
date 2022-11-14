3384d7284c65255bf125030b319347a32dab6e4b
AndresV29 <andresvillalobos.carballo920@gmail.com>
2022-11-13 20:23:46 -0600

    añadir styles con el css no funciona

---
+++ project1_HTML/ActFisica.js
@@ -0,0 +1,5 @@
+let nombre = document.getElementById("nombre");
+let hInicio = document.getElementById("hInicio");
+let hFinal = document.getElementById("hFinal");
+let peso = document.getElementById("peso");
+let imc = document.getElementById("imc");
--- project1_JS/ActFisica.js
+++
@@ -1,8 +0,0 @@
-//Registro de datos 
-//variable: nombre ejercicio input(string), hora inicio militar y hora final(int), peso kg(int), imc actual(int), boton registrar.
-//icono: cardio, fuerza, yoga.
-let nombre = "";
-let horaInicio = 0000;
-let horaFinal = 0000;
-let peso = 0;
-let imc = 0;
