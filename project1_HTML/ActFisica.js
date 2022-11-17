4dd5c888d2ebc556fd4174816bbb4153c6d41f10
AndresV29 <andresvillalobos.carballo920@gmail.com>
2022-11-16 18:22:23 -0600

    parte para presentacion

--- project1_HTML/ActFisica.js
+++ project1_HTML/ActFisica.js
@@ -1,5 +1,3 @@
-let nombre = document.getElementById("nombre");
-let hInicio = document.getElementById("hInicio");
-let hFinal = document.getElementById("hFinal");
-let peso = document.getElementById("peso");
-let imc = document.getElementById("imc");
+document.getElementById("registro").addEventListener("click", function() {
+    alert("Se han registrado los datos de manera exitosa")
+})
