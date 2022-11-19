88c655a2619e2a6d44d6bfd503fea4b6c1764129
AndresV29 <andresvillalobos.carballo920@gmail.com>
2022-11-19 17:26:19 -0600

    se completó pagina act fisica

--- javascript/actFisics.js
+++ javascript/actFisics.js
@@ -1,11 +1,49 @@
-document.getElementById("registro").addEventListener("click", function() {
-    alert("Se han registrado los datos de manera exitosa")
-})
+const hInicio = document.getElementById("h-inicio");
+const hFinal = document.getElementById("h-final");
+const nombre = document.getElementById("nombre");
+const peso = document.getElementById("peso");
+const imc = document.getElementById("imc");
+const btnRegistro = document.getElementById("registro")
 
-function validateForm() {
-    let x = document.forms["myForm"]["nombre"].value;
-    if (x == "") {
-        alert("Debe proveer un nombre para poder registrar una actividad");
-        return false;
+function validaInfo() {
+    if (hInicio.value === "") {
+        alert("Datos insuficientes: Hora de inicio");
+        hInicio.classList.add("vacio");
+    } else {
+        alert("Se pudo registrar correctamente");
+        hInicio.classList.remove("vacio");
+    }
+    if (hFinal.value === "") {
+        alert("Datos insuficientes: Hora de finalizado");
+        hFinal.classList.add("vacio");
+    } else {
+        alert("Se pudo registrar correctamente");
+        hFinal.classList.remove("vacio");
+    }
+    if (nombre.value === "") {
+        alert("Datos insuficientes: Nombre del ejercicio");
+        nombre.classList.add("vacio");
+    } else {
+        alert("Se pudo registrar correctamente");
+        nombre.classList.remove("vacio");
+    }
+    if (peso.value === "") {
+        alert("Datos Insuficientes: Peso");
+        peso.classList.add("vacio");
+    } else {
+        alert("Se pudo registrar correctamente");
+        peso.classList.remove("vacio");
+    }
+    if (imc.value === "") {
+        alert("Datos insuficientes: IMC Actual");
+        imc.classList.add("vacio");
+    } else {
+        alert("Se pudo registrar correctamente");
+        imc.classList.remove("vacio");
     }
 }
+//btnRegistro.addEventListener('click', validaInfo);
+
+btnRegistro.addEventListener('click', Event => {
+    validaInfo();
+});
