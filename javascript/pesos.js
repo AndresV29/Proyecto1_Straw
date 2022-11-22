42c0216f829d2c732f6c8083267fdad767c72b94
fschmidtr <118025239+fschmidtr@users.noreply.github.com>
2022-11-21 22:10:30 -0600

    Update pesos.js

--- javascript/pesos.js
+++ javascript/pesos.js
@@ -1,13 +1,57 @@
 const txtFecha = document.getElementById("txt-fecha-registro");
 const txtPeso = document.getElementById("txt-peso-registro");
+const txtEstatura = document.getElementById("txt-estatura");
 const btnRegistrarPeso = document.getElementById("btn-registrar-peso");
 const tblHistorial = document.querySelector("#tbl-pesos tbody");
 let historial = {};
+let graficoPesos = document.getElementById("grafica-pesos");
+
+const calcularImc = () => {
+    let peso = txtPeso.value;
+    let estatura = txtEstatura.value;
+    let imc = peso / Math.pow(estatura, 2);
+
+    return imc.toFixed(2);
+};
+
+const clasificarImc = (imc) => {
+    let clasificacion = "";
+
+    if (imc >= 40) {
+        clasificacion = "obesidad-3";
+    } else {
+        if (imc >= 35) {
+            clasificacion = "obesidad-2";
+        } else {
+            if (imc >= 30) {
+                clasificacion = "obesidad-1";
+            } else {
+                if (imc >= 25) {
+                    clasificacion = "sobrepeso";
+                } else {
+                    if (imc >= 18.5) {
+                        clasificacion = "normal";
+                    } else {
+                        clasificacion = "bajo";
+                    }
+                }
+            }
+        }
+    }
+    return clasificacion;
+};
 
 const imprimirTabla = () => {
+    let clasificacion = clasificarImc(historial.imc);
+
     let fila = tblHistorial.insertRow();
     fila.insertCell().innerText = historial.fecha;
     fila.insertCell().innerText = historial.peso;
+    fila.insertCell().innerText = historial.imc;
+
+    let celdaClasif = fila.insertCell();
+    celdaClasif.innerText = clasificacion;
+    celdaClasif.classList.add(clasificacion);
 };
 
 const validarPesos = () => {
@@ -27,9 +71,18 @@ const validarPesos = () => {
         txtPeso.classList.remove("vacio");
     }
 
+    if (txtEstatura.value == "") {
+        error = true;
+        txtEstatura.classList.add("vacio");
+    } else {
+        txtEstatura.classList.remove("vacio");
+    }
+
     if (error == false) {
         historial.fecha = txtFecha.value;
         historial.peso = txtPeso.value;
+        historial.estatura = txtEstatura.value;
+        historial.imc = calcularImc();
         imprimirTabla();
     } else {
         console.log("Por favor rellene los campos resaltados en rojo");
@@ -37,3 +90,17 @@ const validarPesos = () => {
 };
 
 btnRegistrarPeso.addEventListener("click", validarPesos);
+
+var chart = new Chart(graficoPesos, {
+    type: "line",
+    data: {
+        labels: ["Peso", "Fecha"],
+        datasets: [{
+            label: "Grafica Historial de Pesos",
+            backgroundColor: "rgb(0,0,0,1.0)",
+            borerColor: "rgb(0,255,0)",
+            data: [txtPeso.value, txtFecha.value],
+        }, ],
+    },
+    options: {},
+});
