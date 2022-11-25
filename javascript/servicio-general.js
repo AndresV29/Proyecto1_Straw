36b92421bc0f6170af5b80b734ca93dbd98bc206
geraldlaboral <113404380+geraldlaboral@users.noreply.github.com>
2022-11-25 06:57:16 -0600

    Create servicio-general.js

---
+++ javascript/servicio-general.js
@@ -0,0 +1,9 @@
+let url = 'http://localhost:3000/api'
+const registrarDatos = async(pdatos, pendPoint) => {
+    url += pendPoint;
+    await axios({
+        method: 'post',
+        url: url,
+        data: pdatos
+    });
+};
