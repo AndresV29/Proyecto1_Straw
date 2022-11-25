7a9e845004f342d952abb0e7e6025297e5e3e1a4
geraldlaboral <113404380+geraldlaboral@users.noreply.github.com>
2022-11-25 12:28:11 -0600

    Update servicio-general.js

--- javascript/servicio-general.js
+++ javascript/servicio-general.js
@@ -1,9 +1,22 @@
 const registrarDatos = async(pdatos, pendPoint) => {
     let url = `http://localhost:3000/api${pendPoint}`
-    url += pendPoint;
     await axios({
         method: 'post',
         url: url,
         data: pdatos
     });
+};
+
+const obtenerDatos = async(pendPoint) => {
+    let url = `http://localhost:3000/api${pendPoint}`
+    let listaDatos = [];
+
+    await axios({
+        method: 'get',
+        url: url
+    }).then(response => {
+        listaDatos = response.data.lista
+    });
+    return listaDatos;
+
 };
