a78178a03f0e82591a8312bf70ff95d19beaab34
geraldlaboral <113404380+geraldlaboral@users.noreply.github.com>
2022-12-07 16:57:47 -0600

    Update perfil.js

--- javascript/perfil.js
+++ javascript/perfil.js
@@ -1,10 +1,31 @@
 moment.locale('es');
 const cuerpoTabla = document.querySelector('#tbl-perfil tbody');
+const fotoPerfil = document.getElementById('fotoPerfil');
 let listaUsuarios = [];
 
 const cargarLista = async() => {
     listaUsuarios = await obtenerDatos('/obtener-usuario');
     mostrarTabla();
+};
+const imagenPhoto = (imagen) => {
+    let img = document.createElement('img');
+    img.src = imagen;
+    return img
+};
+
+const agua = (peso) => {
+    resultado = peso % 7;
+    return `${resultado} vasos de 250 ml`;
+}
+
+
+const proteina = (usuario, estatura) => {
+    if (usuario == 'masculino' && estatura < 52) {
+        resultado = '200 gramos';
+    } else {
+        resultado = '150 gramos';
+    }
+    return resultado;
 };
 
 const mostrarTabla = () => {
@@ -16,8 +37,12 @@ const mostrarTabla = () => {
         fila.insertCell().innerText = usuario.estatura;
         fila.insertCell().innerText = usuario.genero;
         fila.insertCell().innerText = usuario.correo;
-        fila.insertCell().innerText = usuario.foto;
         fila.insertCell().innerText = usuario.pesoMeta;
+        fila.insertCell().innerText = agua(usuario.pesoMeta);
+        fila.insertCell().innerText = proteina(usuario.genero, usuario.estatura);
+        fila.insertCell().innerText = usuario.imcdesdeotrapagina;
+        fila.insertCell().innerText = usuario.actividad;
+        fila.insertCell().appendChild(imagenPhoto(usuario.foto))
     });
 };
 
