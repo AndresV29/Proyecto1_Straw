d699b44e79ba90ddfd9643fee1884652bf1ca774
AndresV29 <andresvillalobos.carballo920@gmail.com>
2022-12-07 20:14:42 -0600

    ultimas partes

--- javascript/actFisics.js
+++ javascript/actFisics.js
@@ -1,58 +1,92 @@
 const hInicio = document.getElementById("h-inicio");
 const hFinal = document.getElementById("h-final");
 const nombre = document.getElementById("nombre");
+const tipo = document.getElementById("tipo");
 const peso = document.getElementById("peso");
 const imc = document.getElementById("imc");
 const btnRegistro = document.getElementById("registro")
 
-let todoBien = false;
+let error = false;
+const cuerpoTabla = document.querySelector("#tbl-actividades tbody");
+let listaActividades = [];
+
+const cargarLista = async() => {
+    listaActividades = await obtenerDatos("/obtener-actividades");
+    mostrarActividades();
+
+};
+
+const mostrarActividades = () => {
+    cuerpoTabla.innerHTML = "";
+    listaActividades.forEach(actividad => {
+        let fila = cuerpoTabla.insertRow();
+        fila.insertCell().innerText = actividad.horaInicial
+        fila.insertCell().innerText = actividad.horaFinal;
+        fila.insertCell().innerText = actividad.nombreActividad;
+        fila.insertCell().innerText = actividad.tipoActividad;
+        fila.insertCell().innerText = actividad.peso;
+        fila.insertCell().innerText = actividad.imc;
+    });
+};
+
+cargarLista();
 
 function validaHoraInicio() {
     if (hInicio.value === "") {
-        todoBien = false;
+        error = false;
         hInicio.classList.add("vacio");
     } else {
-        todoBien = true;
+        error = true;
         hInicio.classList.remove("vacio");
     }
 }
 
 function validaHoraFinal() {
     if (hFinal.value === "") {
-        todoBien = false;
+        error = false;
         hFinal.classList.add("vacio");
     } else {
-        todoBien = true;
+        error = true;
         hFinal.classList.remove("vacio");
     }
 }
 
 function validaNombre() {
     if (nombre.value === "") {
-        todoBien = false;
+        error = false;
         nombre.classList.add("vacio");
     } else {
-        todoBien = true;
+        error = true;
         nombre.classList.remove("vacio");
+    }
+}
+
+function validaTipo() {
+    if (tipo.value === "") {
+        error = false;
+        hInicio.classList.add("vacio");
+    } else {
+        error = true;
+        hInicio.classList.remove("vacio");
     }
 }
 
 function validaPeso() {
     if (peso.value === "") {
-        todoBien = false;
+        error = false;
         peso.classList.add("vacio");
     } else {
-        todoBien = true;
+        error = true;
         peso.classList.remove("vacio");
     }
 }
 
 function validaIMC() {
     if (imc.value === "") {
-        todoBien = false;
+        error = false;
         imc.classList.add("vacio");
     } else {
-        todoBien = true;
+        error = true;
         imc.classList.remove("vacio");
     }
 }
@@ -63,11 +97,22 @@ function validaInfo() {
     validaNombre();
     validaPeso();
     validaIMC();
-    if (todoBien) {
-        //aqui deberia hacer la insercion en la base de datos con los datos que trae con getElementByID
-        alert("esta alarma sonara siempre que todo este bien")
+    if (error == false) {
+        actividad.horaInicial = hInicio.value;
+        actividad.horaFinal = hFinal.value;
+        actividad.nombreActividad = nombre.value;
+        actividad.tipoActividad = tipo.value;
+        actividad.peso = peso.value;
+        actividad.imc = imc.value;
         limpiaTabla();
-    }
+    } else {
+        swal.fire({
+            "icon": "warning",
+            "title": "No se ha registrado la actividad",
+            "text": "Revise los campos resaltados"
+        })
+    };
+    registrarDatos(actividad, "/registrar-actividad");
 }
 
 function limpiaTabla() {
@@ -80,4 +125,34 @@ function limpiaTabla() {
 
 btnRegistro.addEventListener('click', Event => {
     validaInfo();
+});
+
+//codigo de chart.js
+const ctx = document.getElementById('myChart');
+
+var etiquetas = ['Correr', 'Pesas', 'Crosfit', 'Fuerza', 'Ciclismo', 'Yoga', 'Natacion'];
+var datos = [12, 19, 3, 5, 2, 3, 14]
+
+
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: etiquetas,
+        datasets: [{
+            label: 'Actividad fisica',
+            fill: true,
+            borderColor: "#DA3D1B",
+            data: datos,
+            borderWidth: 5,
+
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
