42e1fc5186e6c1d2fa706a09f5f448a8d45d2aba
Andonex <andony1896@gmail.com>
2022-12-08 21:25:06 -0600

    Create mis-recetas.js

---
+++ javascript/mis-recetas.js
@@ -0,0 +1,41 @@
+const filtroTipoComida = document.getElementById("filtro-tipo-comida");
+const filtroCatComida = document.getElementById("filtro-categoria-receta");
+const cuerpoTabla = document.querySelector("#tbl-recetas tbody");
+let listaRecetas = [];
+
+const cargarLista = async() => {
+    listaRecetas = await obtenerDatos("/obtener-recetas");
+    mostrarRecetas();
+
+};
+
+const mostrarRecetas = () => {
+    cuerpoTabla.innerHTML = "";
+    const copiaLista = [...listaRecetas]
+    let listaFiltrada;
+    if (filtroTipoComida.value != "") {
+        listaFiltrada = copiaLista.filter((receta) => {
+            return receta.tipoComida === filtroTipoComida.value
+        });
+    } else if (filtroCatComida.value != "") {
+        listaFiltrada = copiaLista.filter((receta) => {
+            return receta.categoriaComida === filtroCatComida.value
+        });
+    } else {
+        listaFiltrada = copiaLista
+    };
+
+    listaFiltrada.forEach(receta => {
+        let fila = cuerpoTabla.insertRow();
+        fila.insertCell().innerText = receta.nombre;
+        fila.insertCell().innerText = receta.ingredientes;
+        fila.insertCell().innerText = receta.pasosReceta;
+        fila.insertCell().innerText = receta.tipoComida;
+        fila.insertCell().innerText = receta.categoriaComida;
+        fila.insertCell().innerText = receta.imagen;
+    });
+};
+
+cargarLista();
+
+filtroTipoComida.addEventListener("change", mostrarRecetas);
