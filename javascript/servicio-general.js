628ddb6ed7bd8d4f2b6fd784bd0421d224e5b9d3
Andonex <andony1896@gmail.com>
2022-12-09 18:57:12 -0600

    Update servicio-general.js

--- javascript/servicio-general.js
+++ javascript/servicio-general.js
@@ -1,9 +1,25 @@
-const registrarDatos = async(pdatos, pendPoint) => {
+const registrarDatos = async(pdatos, pendPoint, redireccion) => {
     let url = `http://localhost:3000/api${pendPoint}`
     await axios({
         method: 'post',
         url: url,
         data: pdatos
+    }).then(response => {
+        Swal.fire({
+            'icon': 'success',
+            'title': 'Registro exitoso',
+            'text': response.data.msj
+
+        }).then(() => {
+            window.location.href = redireccion;
+        });
+
+    }).catch(error => {
+        Swal.fire({
+            'icon': 'error',
+            'title': 'Ha ocurrido un error',
+            'text': error
+        })
     });
 };
 
