213673c22e681ec5a1c51e0eeb1e99287ae09940
fschmidtr <118025239+fschmidtr@users.noreply.github.com>
2022-12-11 09:09:59 -0600

    Update pesos.js

--- javascript/pesos.js
+++ javascript/pesos.js
@@ -105,7 +105,7 @@ const validarPesos = () => {
             estatura: txtEstatura.value,
         };
 
-        registrarDatos(nuevoPeso, "/registrar-peso");
+        registrarDatos(nuevoPeso, "/registrar-peso", "pesos.html");
     }
 };
 
