9c3e85b08f2bcf46121063f0f4d154032f60c440
geraldlaboral <113404380+geraldlaboral@users.noreply.github.com>
2022-12-12 12:47:11 -0600

    Update editar-perfil.js

--- javascript/editar-perfil.js
+++ javascript/editar-perfil.js
@@ -22,62 +22,62 @@ const limpiar = () => {
     };
 };
 const validar = () => {
-    let error = false;
-    if (txtNombre.value == "") {
-        error = true;
-        txtNombre.classList.add("vacio");
-    } else {
-        txtNombre.classList.remove("vacio");
-    }
-    if (txtFechaNacimiento.value == "") {
-        error = true;
-        txtFechaNacimiento.classList.add("vacio");
-    } else {
-        txtFechaNacimiento.classList.remove("vacio");
-    }
-    if (txtEstatura.value == "") {
-        error = true;
-        txtEstatura.classList.add("vacio");
-    } else {
-        txtEstatura.classList.remove("vacio");
-    }
-    if (txtGenero.value == "") {
-        error = true;
-        txtGenero.classList.add("vacio");
-    } else {
-        txtGenero.classList.remove("vacio");
-    }
-    if (txtCorreo.value == "") {
-        error = true;
-        txtCorreo.classList.add("vacio");
-    } else {
-        txtCorreo.classList.remove("vacio");
-    }
-    if (txtActividad.value == "") {
-        error = true;
-        txtActividad.classList.add("vacio");
-    } else {
-        txtActividad.classList.remove("vacio");
-    }
-    // if (txtFoto.value == "") {
-    //     error = true;
-    //     txtFoto.classList.add("vacio");
-    // } else {
-    //     txtFoto.classList.remove("vacio");
-    // }
-    if (txtPesoMeta.value == "") {
-        error = true;
-        txtPesoMeta.classList.add("vacio");
-    } else {
-        txtPesoMeta.classList.remove("vacio");
-    }
-    if (error) {
-        Swal.fire({
-            icon: "error",
-            title: "No es posible avanzar",
-            text: "Favor rellene los campos resaltados",
-        });
-    } else {
+        // let error = false;
+        // if (txtNombre.value == "") {
+        //     error = true;
+        //     txtNombre.classList.add("vacio");
+        // } else {
+        //     txtNombre.classList.remove("vacio");
+        // }
+        // if (txtFechaNacimiento.value == "") {
+        //     error = true;
+        //     txtFechaNacimiento.classList.add("vacio");
+        // } else {
+        //     txtFechaNacimiento.classList.remove("vacio");
+        // }
+        // if (txtEstatura.value == "") {
+        //     error = true;
+        //     txtEstatura.classList.add("vacio");
+        // } else {
+        //     txtEstatura.classList.remove("vacio");
+        // }
+        // if (txtGenero.value == "") {
+        //     error = true;
+        //     txtGenero.classList.add("vacio");
+        // } else {
+        //     txtGenero.classList.remove("vacio");
+        // }
+        // if (txtCorreo.value == "") {
+        //     error = true;
+        //     txtCorreo.classList.add("vacio");
+        // } else {
+        //     txtCorreo.classList.remove("vacio");
+        // }
+        // if (txtActividad.value == "") {
+        //     error = true;
+        //     txtActividad.classList.add("vacio");
+        // } else {
+        //     txtActividad.classList.remove("vacio");
+        // }
+        // // if (txtFoto.value == "") {
+        // //     error = true;
+        // //     txtFoto.classList.add("vacio");
+        // // } else {
+        // //     txtFoto.classList.remove("vacio");
+        // // }
+        // if (txtPesoMeta.value == "") {
+        //     error = true;
+        //     txtPesoMeta.classList.add("vacio");
+        // } else {
+        //     txtPesoMeta.classList.remove("vacio");
+        // }
+        // if (error) {
+        //     Swal.fire({
+        //         icon: "error",
+        //         title: "No es posible avanzar",
+        //         text: "Favor rellene los campos resaltados",
+        //     });
+        // } else {
         let usuario = {
             nombre: txtNombre.value,
             nacimiento: txtFechaNacimiento.value,
@@ -88,8 +88,8 @@ const validar = () => {
             pesoMeta: txtPesoMeta.value,
             actividad: txtActividad.value,
         };
-        registrarDatos(usuario, "/registro-usuario", "pesos.html");
+        registrarDatos(usuario, "/registro-usuario", "perfil.html");
         limpiar();
     }
-};
+    // };
 btnRegistrar.addEventListener("click", validar);
