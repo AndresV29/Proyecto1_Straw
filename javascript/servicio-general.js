63e36362f2c37ec1724725bd0c3ca0a4c3a77f2f
geraldlaboral <113404380+geraldlaboral@users.noreply.github.com>
2022-12-12 17:08:48 -0600

    Update servicio-general.js

--- javascript/servicio-general.js
+++ javascript/servicio-general.js
@@ -34,3 +34,45 @@ const obtenerDatos = async(pendPoint) => {
     });
     return listaDatos;
 };
+
+const obtenerPersonaId = async(identificacion) => {
+    try {
+        const response = await axios({
+            method: 'get',
+            params: { identificacion: identificacion },
+            url: `http://localhost:3000/api/buscar-persona-identificacion`,
+            responseType: 'json'
+        })
+        return response.data.persona;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const modificarPersona = async(p_id, pdatos, pendPoint) => {
+    let url = `http://localhost:3000/api${pendPoint}`
+    await axios({
+        method: 'put',
+        url: url,
+        data: pdatos,
+        id: p_id,
+
+    }).then(response => {
+        Swal.fire({
+            'icon': 'success',
+            'title': 'Edición exitosa',
+            'text': response.data.msj
+
+        }).then(() => {
+            window.location.href = '../html/perfil.html';
+        });
+
+    }).catch(error => {
+        Swal.fire({
+            'icon': 'error',
+            'title': 'Ha ocurrido un error',
+            'text': error
+        })
+    });
+
+};
